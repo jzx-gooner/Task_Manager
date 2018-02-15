d4934cf91dd422cfcb560819b9a0122453c4e0ab
Unknown <s165308@student.pg.gda.pl>
2018-02-15 21:10:03 +0100

    Add temperature chart
    Add temperature per core chart

--- Task_Manager/Main.py
+++ Task_Manager/Main.py
@@ -1,9 +1,9 @@
-import numpy as np
 from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.widgets import Button
 import psutil
+
 
 class Processor_Data(object):
 
@@ -50,16 +50,22 @@ class Processor_Data(object):
             return 'red'
         elif value == 5:
             return 'grey'
+        elif value==6:
+            return 'brown'
+        elif value==7:
+            return 'orange'
+
 
 class Memory_Data(object):
+
     def __init__(self,bar,ax):
         self.bar=bar
         self.ax=ax
 
     def update_data(self,data):
         self.bar.set_height(data[2])
-        self.ax.text(0.85, 20, 'Current RAM usage: '+ str(data[2])+ '%  ' , style='italic',
-                bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 10})
+        self.ax.text(0.85, 20, 'Current RAM usage: '+ str(data[2])+ '% ' , style='italic',
+                bbox={'facecolor': 'red', 'alpha': 0.6, 'pad': 10})
         ram_used=data[3]
         ram_used=round(data[3]/1000000000,2)
         self.ax.text(0.85, 10, 'Current RAM usage: ' + str(ram_used) + 'GB', style='italic',
@@ -70,18 +76,48 @@ class Memory_Data(object):
         while True:
             yield psutil.virtual_memory()
 
+class Temperature_Data(object):
+
+    def __init__(self,bar,ax):
+        self.bar=bar
+        self.ax=ax
+        self.thread_num=psutil.cpu_count(logical=False)
+
+    def update_data(self,data):
+        for x in range (self.thread_num):
+            tmp=data['coretemp'][x+1][1]
+            self.ax.text(0.15+x, 10, 'Core: ' +str(x)+' temperature= '+ str(tmp) + ' C', style='italic',
+                         bbox={'facecolor': 'red', 'alpha': 0.25, 'pad': 5})
+            self.bar[x][0].set_height(tmp)
+            if tmp >80:
+                self.bar[x][0].set_color('y')
+                if tmp> 90:
+                    self.bar[x][0].set_color('r')
+            else:
+                self.bar[x][0].set_color('g')
+        return self.bar
+
+    def get_data(self):
+        while True:
+            yield psutil.sensors_temperatures()
+
+
 class Manager(object):
 
     def __init__(self):
         self.fig, self.ax = plt.subplots()
         self.fig.set_size_inches(7.5, 7.5)
         self.ani = None
-        m_button = plt.axes([0.4, 0.92, 0.1, 0.05])
-        c_button = plt.axes([0.5, 0.92, 0.1, 0.05])
+        m_button = plt.axes([0.35, 0.92, 0.1, 0.05])
+        c_button = plt.axes([0.45, 0.92, 0.1, 0.05])
+        t_button = plt.axes([0.55, 0.92, 0.1, 0.05])
+        self.fig.canvas.set_window_title('Task Manager')
         memory_button = Button(m_button,'Memory')
         cpu_button = Button(c_button,'CPU')
+        temp_button = Button(t_button,'t of CPU')
         cpu_button.on_clicked(self.cpu_clicked)
         memory_button.on_clicked(self.mem_clicked)
+        temp_button.on_clicked(self.temp_clicked)
         plt.show()
 
     def cpu_clicked(self,event):
@@ -96,8 +132,7 @@ class Manager(object):
         self.ax.set_xlabel('Time')
         self.ax.set_ylabel('Percent usage')
         proc = Processor_Data(self.ax, threads_number)
-        self.ani = animation.FuncAnimation(self.fig, proc.update_data, proc.get_data, interval=10,
-                                      blit=False)
+        self.ani = animation.FuncAnimation(self.fig, proc.update_data, proc.get_data, interval=10)
         plt.show()
 
     def mem_clicked(self,event):
@@ -107,11 +142,30 @@ class Manager(object):
         self.ax.cla()
         self.ax.set_title('MEMORY USAGE')
         self.ax.set_ylim(0,100)
+        self.ax.set_xlim(0.5,1.5)
         self.ax.set_ylabel('Percent usage')
         rect, = self.ax.bar(1,0,color='blue')
         mem=Memory_Data(rect,self.ax)
         self.ani = animation.FuncAnimation(self.fig,mem.update_data, mem.get_data, interval=100)
         plt.show()
 
+    def temp_clicked(self,event):
 
-manager = Manager()
+        if self.ani != None:
+            self.ani.event_source.stop()
+        self.ax.cla()
+        self.ax.set_title('Temperature per core')
+        self.ax.set_ylim(0, 100)
+        x_size=psutil.cpu_count(logical=False)
+        self.ax.set_xlim(0,x_size)
+        self.ax.set_ylabel('Temperature in Celsius')
+        bars =[]
+        for x in range (psutil.cpu_count(logical=False)):
+            bars.append(self.ax.bar(x+0.5,0,color='green'))
+
+        temp=Temperature_Data(bars,self.ax)
+        self.ani= animation.FuncAnimation(self.fig,temp.update_data, temp.get_data, interval=100)
+        plt.show()
+
+if __name__ == "__main__":
+    manager = Manager()
